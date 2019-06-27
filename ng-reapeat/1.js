8b44d9877c03657a857cf54a9b2c006073431c61
devra1n <devra1n@outlook.com>
2019-06-27 15:38:33 +0700

    upload ng-repeat

---
+++ ng-reapeat/1.js
@@ -0,0 +1,39 @@
+﻿ var app = angular.module('myApp',['ngMaterial']);
+ app.controller('MyController',  function($scope){
+  	
+
+
+
+  	$scope.nhieunguoi = [
+  		{
+  			name : "Viet",
+  			age : "19",
+  			facebook : "facebook.com/devra1n",
+  			sdt : "0976528849"
+  		},
+  		{
+  			name : "Duc",
+  			age : "23",
+  			facebook : "facebook.com/devra2n",
+  			sdt : "0976528849"
+  		},
+  		{
+  			name : "Kien",
+  			age : "20",
+  			facebook : "facebook.com/devra3n",
+  			sdt : "0976528849"
+  		},
+  		{
+  			name : "Khanh",
+  			age : "29",
+  			facebook : "facebook.com/devra4n",
+  			sdt : "0976528849"
+  		}
+
+
+  		]
+  		
+  		
+
+  	
+ })
